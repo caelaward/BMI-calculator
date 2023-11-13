68fec8622a33ffba1ca09df8a7c5288fb0ea69fd
caelaward <callaward32@gmail.com>
2023-11-13 23:09:47 +0200

    added changes

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ let w = document.querySelector ('[data-weight]').value;
 let btn = document.querySelector('[data-btn]')
 
 
+
 btn.addEventListener('click', function(){
 let h = document.querySelector('[data-height]').value;
 let w = document.querySelector ('[data-weight]').value;
+
+
  BMI(w,h);
 
 })
@@ -14,5 +17,6 @@ let w = document.querySelector ('[data-weight]').value;
     function BMI(w,h) {
         let answer= w/(h*h)
         console.log(answer);
-        document.querySelector('[data-answer]').value = answer.toFixed(2)
+        document.querySelector('[data-answer]').value = answer.toFixed(2);
     }
+
